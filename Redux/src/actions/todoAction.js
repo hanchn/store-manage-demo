884737a1cd19123aa770678f232416f40d0b338f
hanchn <jing.yuan@kapeixi.com>
2023-11-09 15:23:39 +0800

    .

---
+++ Redux/src/actions/todoAction.js
@@ -0,0 +1,23 @@
+const ADD_TODO = 'ADD_TODO'
+const DELETE_TODO = 'DELETE_TODO'
+const EDIT_TODO = 'EDIT_TODO'
+const COMPLETE_TODO = 'COMPLETE_TODO'
+const COMPLETE_ALL = 'COMPLETE_ALL'
+const CLEAR_COMPLETED = 'CLEAR_COMPLETED'
+const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+const SHOW_ALL = 'SHOW_ALL'
+const SHOW_ACTIVE = 'SHOW_ACTIVE'
+const SHOW_COMPLETED = 'SHOW_COMPLETED'
+const SHOW_PENDING = 'SHOW_PENDING'
+const SHOW_OVERDUE = 'SHOW_OVERDUE'
+
+
+const addTodo = (text) => { 
+  ...text
+}
+
+const removeTodo = (id) => id
+
+const updateTodo = (text) => { 
+  ...text
+}
